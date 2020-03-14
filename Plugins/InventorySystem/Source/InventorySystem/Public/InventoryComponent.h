4b5a4e2d262cb0ee9c65a63cdc444d6766bb0883
jeremyLee0512 <2410250284@qq.com>
2020-03-14 21:55:23 +0800

    InventorySystem插件

---
+++ Plugins/InventorySystem/Source/InventorySystem/Public/InventoryComponent.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InventoryComponent.generated.h"
+
+class UItemDataAsset;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class INVENTORYSYSTEM_API UInventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UInventoryComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
+
+
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnInventoryItemChangedNative, bool, UItemDataAsset*)
+
---
+++ Plugins/InventorySystem/Source/InventorySystem/Public/ItemDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ItemDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UItemDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};

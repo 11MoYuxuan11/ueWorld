82943c057a80e9237ccb1c34de461889daf2315e
jeremyLee0512 <2410250284@qq.com>
2020-05-30 09:00:53 +0800

    动画资源备份

---
+++ Source/unWorld/Public/Abilities/PlayMontageAndWaitForEvent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask.h"
+#include "PlayMontageAndWaitForEvent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNWORLD_API UPlayMontageAndWaitForEvent : public UAbilityTask
+{
+	GENERATED_BODY()
+	
+};
